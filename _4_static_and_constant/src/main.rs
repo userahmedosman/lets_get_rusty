0b456d0a1526eca7a8b65d94c9e6075df4642155
Hajo <ahmedhaj000@gmail.com>
2026-08-24 06:40:49 +0300

    day 4 statics and constants

---
+++ _4_static_and_constant/src/main.rs
@@ -0,0 +1,10 @@
+const GAME_LEVEL: i8 = 20;
+static GAME_NAME: &str = "Rusty Casino";
+
+fn main() {
+    let glevel = GAME_LEVEL;
+    let gname = GAME_NAME;
+
+    println!("Game name: {gname}");
+    println!("Game Level: {glevel}");    
+}
