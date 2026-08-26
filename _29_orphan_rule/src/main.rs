443b60e19f5641c33c6f9b07d4b3330fda2ee08d
Hajo <ahmedhaj000@gmail.com>
2026-08-26 06:43:23 +0300

    day 29 the orphane rule

---
+++ _29_orphan_rule/src/main.rs
@@ -0,0 +1,14 @@
+use orphan_rule::Point;
+
+struct PointWrapper(Point);
+
+impl PartialEq for PointWrapper{
+    fn eq(&self, other: &Self) -> bool {
+        self.0.x == other.0.x && self.0.y == other.0.y
+    }
+}
+fn main() {
+    let p1 = PointWrapper(Point { x: 1, y: 3 });
+    let p2 = PointWrapper(Point { x: 1, y: 3 });
+    println!("{}", p1 == p2);
+}
